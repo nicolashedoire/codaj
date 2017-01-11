d3cdcab23d7d9dffccb33aa85bb03e07442738c1
nicolashedoire <nicolas@kinomap.com>
2017-01-11 19:02:41 +0100

    add express

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var env = process.env.NODE_ENV;
+var express = require('express');
+var app = express();
 
-var http = require('http');
+
+app.get('/' , (req, res) => {
+
+	res.send('Hello world');
+});
+
+/*var http = require('http');
 var server = http.createServer((req, res)=> {
 	console.log('request on : ' + req.url + ' | Method : ' + req.method);
 	res.writeHead(200 , {'Content-Type' : 'text/html'});
@@ -9,7 +17,7 @@ var server = http.createServer((req, res)=> {
 	}
 	res.end('it works at ' + req.connection.remoteAddress);
 });
-
+*/
 server.listen(process.env.PORT || 3000 , () =>{
 	console.log('works on port : ' + process.env.PORT);
 });
