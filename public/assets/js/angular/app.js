c462ea07bc3f6fda5de6479e6b1fa6a9fa9c65f9
nicolashedoire <nicolas@kinomap.com>
2017-01-12 22:54:08 +0100

    add routing + views + controllers

--- public/assets/js/angular/app.js
+++ public/assets/js/angular/app.js
@@ -1,20 +1,79 @@
-		var app = angular.module("coding" , ['ui.bootstrap'] , ($locationProvider)=> {
+		var app = angular.module("coding" , ['ngRoute' , 'ui.bootstrap']);
+
+	    // configure routes
+	    app.config(function($routeProvider , $locationProvider) {
+
 		    // delete ! prefix in url 
 		    $locationProvider.hashPrefix("");
-		});
+
+	        $routeProvider
+	            .when('/', {
+	                templateUrl : 'views/home.html',
+	                controller  : 'homeController'
+	            })
+	            .when('/dashboard', {
+	                templateUrl : 'views/dashboard.html',
+	                controller  : 'dashboardController'
+	            })
+	            // route for the logout page
+	            .when('/code', {
+	            	templateUrl : 'views/code.html',
+	            	controller : 'codeController'
+	            })
+	            .when('/tests', {
+	                templateUrl  : 'views/tests.html',
+	                controller : 'testsController'
+	            })
+	            .when('/database' , {
+	                templateUrl : 'views/database.html',
+	                controller : 'databaseController'
+	            })
+	            .when('/myaccount' , {
+	                templateUrl : 'views/myaccount.html',
+	                controller : 'myaccountController'
+	            })
+	            .when('/subscriptions' , {
+	                templateUrl : 'views/subscriptions.html',
+	                controller : 'subscriptionsController'
+	            })
+	            .otherwise({ redirectTo: '/login' });
+	    });
 
 
+	    // controllers
+	    // 
 		app.controller("codingCtrl" , function($scope , $location){
-/*		    $scope.menu = menu;
-		    
-		    $scope.currentItem = null;
-		    $scope.selectionMenu = function(item){
-		        $scope.currentItem = item;
-		    }*/
-		    
 		    $scope.$watch(function(){
 		       return $location.path(); 
 		    }, function(newPath){
 		        console.log(newPath);
 		    });
+		});
+
+		app.controller("homeController" , function($scope , $location){
+
+		});
+
+		app.controller("dashboardController" , function($scope , $location){
+
+		});
+
+		app.controller("codeController" , function($scope , $location){
+
+		});
+
+		app.controller("testsController" , function($scope , $location){
+
+		});
+
+		app.controller("databaseController" , function($scope , $location){
+
+		});
+
+		app.controller("myaccountController" , function($scope , $location){
+
+		});
+
+		app.controller("subscriptionsController" , function($scope , $location){
+
 		});
